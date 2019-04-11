501a003584ec7c53b8f1e5ff734d5d7082afe0fa
Robert Willardson Account <roc123223@gmail.com>
2019-04-11 13:43:57 -0600

    Update 4/11

--- assignments/templeinnsite/scripts/temples.js
+++ assignments/templeinnsite/scripts/temples.js
@@ -1,55 +1,44 @@
 var header = document.querySelector('header');
-        var section = document.querySelector('section');
-        var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-        var request = new XMLHttpRequest();
-        request.open('GET', requestURL);
-        request.responseType = 'json';
-        request.send();
-        request.onload = function() {
-            var superHeroes = request.response;
-            populateHeader(superHeroes);
-            showHeroes(superHeroes);
-        }   
+var section = document.querySelector('section');
+var requestURL = 'https://roc123223.github.io/assignments/templeinnsite/JSON/temples.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    var superHeroes = request.response;
+    showHeroes(superHeroes);
+}
 
-        function populateHeader(jsonObj) {
-            var myH1 = document.createElement('h1');
-            myH1.textContent = jsonObj['squadName'];
-            header.appendChild(myH1);
+function showHeroes(jsonObj) {
+    var heroes = jsonObj['members'];
 
-            var myPara = document.createElement('p');
-            myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-            header.appendChild(myPara);
+    for (var i = 0; i < heroes.length; i++) {
+        var myArticle = document.createElement('article');
+        var myH2 = document.createElement('h2');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myList = document.createElement('ul');
+
+        myH2.textContent = heroes[i].name;
+        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+        myPara2.textContent = 'Age: ' + heroes[i].age;
+        myPara3.textContent = 'Superpowers:';
+
+        var superPowers = heroes[i].powers;
+        for (var j = 0; j < superPowers.length; j++) {
+            var listItem = document.createElement('li');
+            listItem.textContent = superPowers[j];
+            myList.appendChild(listItem);
         }
 
-        function showHeroes(jsonObj) {
-            var heroes = jsonObj['members'];
-      
-            for (var i = 0; i < heroes.length; i++) {
-                var myArticle = document.createElement('article');
-                var myH2 = document.createElement('h2');
-                var myPara1 = document.createElement('p');
-                var myPara2 = document.createElement('p');
-                var myPara3 = document.createElement('p');
-                var myList = document.createElement('ul');
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myList);
 
-                myH2.textContent = heroes[i].name;
-                myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-                myPara2.textContent = 'Age: ' + heroes[i].age;
-                myPara3.textContent = 'Superpowers:';
-                    
-                var superPowers = heroes[i].powers;
-                for (var j = 0; j < superPowers.length; j++) {
-                    var listItem = document.createElement('li');
-                    listItem.textContent = superPowers[j];
-                    myList.appendChild(listItem);
-                }
-
-                myArticle.appendChild(myH2);
-                myArticle.appendChild(myPara1);
-                myArticle.appendChild(myPara2);
-                myArticle.appendChild(myPara3);
-                myArticle.appendChild(myList);
-
-                section.appendChild(myArticle);
-            }
-        }
+        section.appendChild(myArticle);
+    }
+}
