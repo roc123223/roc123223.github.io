13bffe11cd129110a2153815e234061635375804
Robert Willardson Account <roc123223@gmail.com>
2019-04-12 09:08:01 -0600

    4

--- assignments/templeinnsite/scripts/temples.js
+++ assignments/templeinnsite/scripts/temples.js
@@ -1,4 +1,3 @@
-var header = document.querySelector('header');
 var section = document.querySelector('section');
 var requestURL = 'https://roc123223.github.io/assignments/templeinnsite/JSON/temples.json';
 var request = new XMLHttpRequest();
@@ -6,40 +5,41 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function () {
-    var superHeroes = request.response;
-    showHeroes(superHeroes);
+    var allTemples = request.response;
+    showTemples(allTemples);
 }
 
-function showHeroes(jsonObj) {
-    var heroes = jsonObj['temples'];
+function showTemples(jsonObj) {
+    var temples = jsonObj['temples'];
 
-    for (var i = 0; i < heroes.length; i++) {
+    for (var i = 0; i < temples.length; i++) {
         var myArticle = document.createElement('article');
         var myH3 = document.createElement('h3');
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
         var myList1 = document.createElement('ul');
-        var myList2 = document.createElement('ul');
 
-        myH3.textContent = heroes[i].name;
-        myPara1.textContent = heroes[i].address;
-        myPara2.textContent = heroes[i].number;
+        myH3.textContent = temples[i].name;
+        myPara1.textContent = temples[i].address;
+        myPara2.textContent = temples[i].number;
         myList1.textContent = "Temple Schedule:"
 
-        var superPowers = heroes[i].schedule;
-        for (var j = 0; j < superPowers.length; j++) {
+        var schedule = temples[i].schedule;
+        for (var j = 0; j < schedule.length; j++) {
             var listItem = document.createElement('li');
-            listItem.textContent = superPowers[j];
+            listItem.textContent = schedule[j];
             myList.appendChild(listItem);
         }
-
+        
+        var myList2 = document.createElement('ul');
         myList2.textContent = "Closures:"
-        var closures = heroes[i].closures;
-        for (var j = 0; j < superPowers.length; j++) {
+        var closures = temples[i].closures;
+        for (var j = 0; j < closures.length; j++) {
             var listItem = document.createElement('li');
-            listItem.textContent = superPowers[j];
+            listItem.textContent = closures[j];
             myList.appendChild(listItem);
         }
+        
 
 
         myArticle.appendChild(myH3);
